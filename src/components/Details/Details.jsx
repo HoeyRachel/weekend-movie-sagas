e6c95c90663463ee52118a40be44bb2f6fb72866
Rachel Hoey <rhoey@hotmail.com>
2021-12-09 18:52:29 -0600

    details page up

---
+++ src/components/Details/Details.jsx
@@ -0,0 +1,19 @@
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import './Details.css'
+
+function Details() {
+
+   
+
+    return (
+        <main>
+            <h1>Movie Details</h1>
+           
+            
+        </main>
+
+    );
+}
+
+export default Details;
